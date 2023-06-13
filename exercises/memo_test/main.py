9645ced292e1a6c256633110b79e8404af456844
streppelandres <andres.streppel@gmai.com>
2023-06-13 19:36:25 -0300

    Dinamic size cards creation and board initialization

--- exercises/memo_test/main.py
+++ exercises/memo_test/main.py
@@ -1,5 +1,6 @@
+import random
 import pygame
-from config import game_cfg, video_cfg
+from config import game_cfg, video_cfg, gameplay_cfg
 from utils import colors
 
 pygame.init()
@@ -9,15 +10,50 @@ clock = pygame.time.Clock()
 delta_time = 0
 is_running = True
 
+# ------- INIT CARDS
+cards = []
+for _ in range(0, gameplay_cfg.NUMBER_OF_CARDS):
+    while True:
+        card = colors.get_random_color()
+        if not card in cards:
+            cards.append(card)
+            break
+
+cards = cards + cards # cards duplication
+random.shuffle(cards)
+
+#print(cards)
+
+# ------- INIT BOARD
+rows = int(len(cards) ** 0.5)
+columns = len(cards) // rows
+
+card_width = video_cfg.WIDTH // (gameplay_cfg.NUMBER_OF_CARDS)
+card_height = video_cfg.HEIGHT // (gameplay_cfg.NUMBER_OF_CARDS // 1.5)
+
+print(f'card_width -> {card_width}, card_height -> {card_height}')
+
+board = []
+index = 0
+for i in range(rows):
+    row = []
+    for j in range(columns):
+        row.append(cards[index])
+        pygame.draw.rect(screen, cards[index], (i * card_width, j * card_height, card_width, card_height))
+        index += 1
+    board.append(row)
+
+#print(board)
+
+# -------------------------------------------
+
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print('cya 👋')
             is_running = False
 
-    screen.fill(colors.BLACK)
-
-    # tutururu
+    # game code here
 
     pygame.display.flip()
     delta_time = clock.tick(video_cfg.FPS) / 1000
